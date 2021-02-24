d5db3193452e0f6a41ec90dda1c549db0f5c6faf
Kübra Eser <Kbra Eser@DESKTOP-CMO2N5V>
2021-02-24 03:10:59 +0300

    .

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class : referans tip
+    //new() : newlenebilir olmalı. IEntity soyut newlenemez.
+    public interface IEntityRepository<T> where T:class, IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -43,9 +44,19 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(ProductToDelete);
         }
 
+        public Product get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
         {
             return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetAllByCategory(int categoryId)
