7a0419be791e7669eccc9241680f1dcbce64eaeb
Kübra Eser <Kbra Eser@DESKTOP-CMO2N5V>
2021-02-26 03:35:48 +0300

    .

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public CategoryManager(EfCategoryDal efCategoryDal)
+        {
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.get(c => c.CategoryId == categoryId);
+        }
+    }
+}
--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICostumerDal:IEntityRepository<Customer>
+    public interface IOrderDal : IEntityRepository<Order>
     {
     }
 }
