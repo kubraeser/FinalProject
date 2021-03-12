857a32ce78005f24c04eedc672b469eb2173f665
Kübra Eser <Kbra Eser@DESKTOP-CMO2N5V>
2021-03-12 16:30:12 +0300

    .

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
         public static string CategoryLimitExceded = "En fazla 15 kategori olabilir.";
+        public static string  AuthorizationDenied = "Yetkiniz yoktur.";
+        public static string UserRegistered = "Kayıt oldu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Hatalı şifre girildi.";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
+        public static string SuccessfulLogin = "Giriş Başarılı.";
+        public static string UserAlreadyExists = "Kullanıcı mevcut.";
     }
 }
